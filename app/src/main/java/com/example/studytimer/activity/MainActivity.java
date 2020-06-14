1d101bcd18910a3a0dc139f210bf1b7307b81b62
MMBorkowski <34061346+MMBorkowski@users.noreply.github.com>
2020-06-15 01:12:01 +0200

    Simple history

--- app/src/main/java/com/example/studytimer/activity/MainActivity.java
+++ app/src/main/java/com/example/studytimer/activity/MainActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.studytimer.R;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/studytimer/adapter/ArrayAdapter.java
@@ -0,0 +1,62 @@
+package com.example.studytimer.adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.studytimer.R;
+import com.example.studytimer.dboperation.dbobjects.Times;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ArrayAdapter extends RecyclerView.Adapter<ArrayAdapter.ViewHolder> {
+
+    //All methods in this adapter are required for a bare minimum recyclerview adapter
+    private int listItemLayout;
+    private ArrayList<Times> itemList;
+    // Constructor of the class
+    public ArrayAdapter(int layoutId, ArrayList<Times> itemList) {
+        listItemLayout = layoutId;
+        this.itemList = itemList;
+    }
+
+    // get the size of the list
+    @Override
+    public int getItemCount() {
+        return itemList == null ? 0 : itemList.size();
+    }
+
+
+    // specify the row layout file and click for each row
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(listItemLayout, parent, false);
+        ViewHolder myViewHolder = new ViewHolder(view);
+        return myViewHolder;
+    }
+
+    // load data in each row element
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int listPosition) {
+        TextView item = holder.item;
+        item.setText(new String(itemList.get(listPosition).getStart().toString()+" "+itemList.get(listPosition).getStop().toString()));
+    }
+
+    // Static inner class to initialize the views of rows
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView item;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            item = (TextView) itemView.findViewById(R.id.row_item);
+        }
+        @Override
+        public void onClick(View view) {
+            Log.d("onclick", "onClick " + getLayoutPosition() + " " + item.getText());
+        }
+    }
+}
