70f04e8d9732c6a41c81b1cabd1073566a0da1c6
MMBorkowski <34061346+MMBorkowski@users.noreply.github.com>
2020-06-14 15:32:15 +0200

    Timer start

--- app/src/main/java/com/example/studytimer/activity/Active.java
+++ app/src/main/java/com/example/studytimer/activity/Active.java
@@ -1,17 +1,72 @@
 package com.example.studytimer.activity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.studytimer.R;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Active extends AppCompatActivity {
+
+    private TextView timeText;
+    private Button controlButton;
+    private CountDownTimer countDownTimer;
+    private boolean isTimerRunning;
+    private long timeLeftinMillis = 60000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active);
+        timeText = (TextView)findViewById(R.id.text_view_countdown);
+        controlButton = (Button)findViewById(R.id.button_stop);
+        controlButton.setText("Start");
     }
 
     public void onStartButton(View view) {
+
+        if(isTimerRunning){
+            pauseTimer();
+        }
+        else{
+            startTimer();
+        }
+        Date currentTime = Calendar.getInstance().getTime();
+        timeText.setText(currentTime.toString());
+    }
+
+    private void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftinMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftinMillis = millisUntilFinished;
+                updateCountdownText();
+                controlButton.setText("Stop");
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        isTimerRunning = true;
+    }
+    private void pauseTimer(){
+
+    }
+
+    private void updateCountdownText(){
+        int minutes = (int) timeLeftinMillis/1000/60;
+        int seconds = (int) timeLeftinMillis/1000 % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d",minutes, seconds);
+        timeText.setText(timeLeftFormatted);
     }
 }
